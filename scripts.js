af9dbb8b6ddfc0bc3f1b340a31bda9f92d55ee29
BuildTools <unconfigured@null.spigotmc.org>
2023-09-23 19:08:20 -0400

    Added sidebar

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function displayMenu() {
+        const x = document.getElementById("navMenu");
+        if (x.style.display === "block") {
+                x.style.display = "none";
+        } else {
+                x.style.display = "block";
+        }
+}
